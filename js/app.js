dffe297402270a9285adf48ac837bedfa73e0c50
rodrigocsar <85592886+rodrigocsar@users.noreply.github.com>
2022-06-01 20:50:54 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,57 @@
+// Chamada api
+let productPromise = fetch(
+  "http://makeup-api.herokuapp.com/api/v1/products.json"
+);
+productPromise.then((resp) => {
+  resp.json().then((product) => {
+    let productInfo = productItem(product);
+    document.getElementById("app").innerHTML = productInfo;
+  });
+});
+
+//EXEMPLO DO CÓDIGO PARA UM PRODUTO
+function productItem(product) {
+  const item = product.map((products) => {
+    return `<div class="product" data-name="NYX Mosaic Powder Blush Paradise" data-brand="nyx" data-type="bronzer" tabindex="508">
+  <figure class="product-figure">
+    <img src="https://d3t32hsnjxo7q6.cloudfront.net/i/deedb7bd74bda43f062a09aab2ee1ec8_ra,w158,h184_pa,w158,h184.png" width="215" height="215" alt="NYX Mosaic Powder Blush Paradise" onerror="javascript:this.src='img/unavailable.png'">
+  </figure>
+  <section class="product-description">
+    <h1 class="product-name">${products.name}</h1>
+    <div class="product-brands"><span class="product-brand background-brand">${products.brand}</span>
+<span class="product-brand background-price">R$ ${products.price}</span></div>
+  </section>
+</div>`;
+  });
+  return item;
+}
+
+//EXEMPLO DO CÓDIGO PARA OS DETALHES DE UM PRODUTO
+function loadDetails(product) {
+  let details = `<section class="product-details"><div class="details-row">
+        <div>Brand</div>
+        <div class="details-bar">
+          <div class="details-bar-bg" style="width= 250">nyx</div>
+        </div>
+      </div><div class="details-row">
+        <div>Price</div>
+        <div class="details-bar">
+          <div class="details-bar-bg" style="width= 250">10.49</div>
+        </div>
+      </div><div class="details-row">
+        <div>Rating</div>
+        <div class="details-bar">
+          <div class="details-bar-bg" style="width= 250">5</div>
+        </div>
+      </div><div class="details-row">
+        <div>Category</div>
+        <div class="details-bar">
+          <div class="details-bar-bg" style="width= 250"></div>
+        </div>
+      </div><div class="details-row">
+        <div>Product_type</div>
+        <div class="details-bar">
+          <div class="details-bar-bg" style="width= 250">bronzer</div>
+        </div>
+      </div></section>`;
+}
